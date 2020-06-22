024bf3e9c89ab6b89af545cafd6a6dd5bf10fed7
gero007 <gero007@gmail.com>
2020-06-22 10:46:45 +0200

    out-of-the-lockdown sync

--- testHolt.R
+++ testHolt.R
@@ -57,21 +57,21 @@ getSpotPredDiso <- function(df,output,accession_col){
 
 
 # ----------------------------------------Data Prep-------------------------------------------------------------
-holtPsitesTable <- Holt_MS %>% group_by(Uniprot) %>% summarise(sites=paste(substring(site,2,1000), collapse=","))
-holtCDK1PsitesTable <- Holt_MS_Cdk1_all_phosphosites %>% group_by(Uniprot) %>% summarise(sites=paste(substring(site,2,1000), collapse=","))
-holtCDK1PsitesConsTable <- Holt_MS_Cdk1_consensus_phosphosites %>% group_by(Uniprot) %>% summarise(sites=paste(substring(site,2,1000), collapse=","))
+holtPsitesTable <- holt_MS %>% group_by(Uniprot) %>% summarise(sites=paste(substring(site,2,1000), collapse=","))
+holtCDK1PsitesTable <- holt_MS_Cdk1_all_phosphosites %>% group_by(Uniprot) %>% summarise(sites=paste(substring(site,2,1000), collapse=","))
+holtCDK1PsitesConsTable <- holt_MS_Cdk1_consensus_phosphosites %>% group_by(Uniprot) %>% summarise(sites=paste(substring(site,2,1000), collapse=","))
 #The contingency table analysis it only make sense in the CDK1 targets if I am comparing T/S phosphorylated in al conditions vs lost T/S phosphorylation upon cdk1 inhibition. Non cdk1 targets don't have lost T/S phosphorylation which makes the first row of the contingency table empty
 
-HoltAllPsites <- merge.data.frame(holtPsitesTable,holtCDK1PsitesTable,by = "Uniprot",all.x = T)
-HoltAllPsites <- merge.data.frame(HoltAllPsites,holtCDK1PsitesConsTable,by = "Uniprot",all.x = T)
+holtAllPsites <- merge.data.frame(holtPsitesTable,holtCDK1PsitesTable,by = "Uniprot",all.x = T)
+holtAllPsites <- merge.data.frame(HoltAllPsites,holtCDK1PsitesConsTable,by = "Uniprot",all.x = T)
   
 colnames(HoltAllPsites)[2:4] <- c("Psites","Cdk1Psites","Cdk1PsitesCons")
-HoltAllPsites$Psites<-strsplit(HoltAllPsites$Psites,",")
+holtAllPsites$Psites<-strsplit(holtAllPsites$Psites,",")
 
-HoltAllPsites$Cdk1Psites<-strsplit(HoltAllPsites$Cdk1Psites,",")
+holtAllPsites$Cdk1Psites<-strsplit(holtAllPsites$Cdk1Psites,",")
 # HoltAllPsites$Cdk1Psites[is.na(HoltAllPsites$Cdk1Psites)]<-0
 
-HoltAllPsites$Cdk1PsitesCons<-strsplit(HoltAllPsites$Cdk1PsitesCons,",")
+holtAllPsites$Cdk1PsitesCons<-strsplit(holtAllPsites$Cdk1PsitesCons,",")
 # HoltAllPsites$Cdk1PsitesCons[is.na(HoltAllPsites$Cdk1PsitesCons)]<-0
 # ----------------------------------------------------------------------------------------------------------------
 # Data prep could be added to the first section of the Index.Rmd
@@ -80,6 +80,9 @@ HoltAllPsites$Cdk1PsitesCons<-strsplit(HoltAllPsites$Cdk1PsitesCons,",")
 
 Holt_indexes_data <- Holt_data[,c(1,3,2,13)]
 Holt_indexes_data$SPOT<- getSpotPredDiso(Holt_data,accession_col = "Uniprot",output = "indices")
+Holt_indexes_data$SPOTperc<- getSpotPredDiso(Holt_data,accession_col = "Uniprot",output = "percentage")
+Holt_indexes_data$Lite<- getPredLiteDiso(Holt_data,accession_col = "Uniprot",output = "indices")
+Holt_indexes_data$Liteperc<- getPredLiteDiso(Holt_data,accession_col = "Uniprot",output = "percentage")
 Holt_indexes_data <- merge.data.frame(Holt_indexes_data,HoltAllPsites,by="Uniprot",all.x = T)
 Holt_indexes_data$Psites<-lapply(Holt_indexes_data$Psites,as.numeric)
 Holt_indexes_data$Cdk1Psites<-lapply(Holt_indexes_data$Cdk1Psites,as.numeric)
@@ -111,6 +114,46 @@ for (i in 1:nrow(Holt_indexes_data_CDK1)) {
   stratContingencyArray[2,2,i]<-Ord_NONPhos
   }
 
+
+# Individual fisher tests
+
+
+
+
+Holt_indexes_data_CDK1$FisherOR <- apply(stratContingencyArray, 3,function(x){
+  ft<-fisher.test(x)
+  return(ft$estimate)
+})
+Holt_indexes_data_CDK1$FisherPval <- apply(stratContingencyArray, 3,function(x){
+  ft<-fisher.test(x)
+  return(ft$p.value)
+})
+Holt_indexes_data_CDK1$FisherAdjPval <- p.adjust(Holt_indexes_data_CDK1$FisherPval,method = "BH")
+
+Holt_indexes_data_CDK1$FisherlogPvalue <- -log10(Holt_indexes_data_CDK1$FisherPval)
+Holt_indexes_data_CDK1$FisherlogAdjPvalue <- -log10(Holt_indexes_data_CDK1$FisherAdjPval)
+Holt_indexes_data_CDK1$FisherlogOr <- log10(Holt_indexes_data_CDK1$FisherOR)
+
+fisherSpotDF$logOr[is.infinite(fisherSpotDF$logOr) & fisherSpotDF$logOr > 0] <- 2
+fisherSpotDF$logOr[is.infinite(fisherSpotDF$logOr) & fisherSpotDF$logOr < 0] <- -2
+
+
+###Trying to find a way to move the dots out from the logPvalue=0 with random noise(Jitter) in order to asses the amount of points we have there.
+set.seed(1234)
+fisherSpotDF$logPvalue[fisherSpotDF$logPvalue==0]<- 0.1
+fisherSpotDF$logPvalue[fisherSpotDF$logPvalue==0.1]<-abs(jitter(fisherSpotDF$logPvalue[fisherSpotDF$logPvalue==0.1],6))
+
+ggplot(fisherSpotDF, aes(x = logOr, y = logPvalue)) +
+  geom_point(aes(color = target)) +
+  # scale_color_manual(values = c("", "grey")) +
+  theme_bw(base_size = 12) + theme(legend.position = "bottom") +
+  scale_y_continuous(trans='log10',limits = c(0.01,10)) +
+  xlim(-2, 2) +
+  scale_shape_manual()+
+  # geom_point(data = subset(fisherDF, log10(ORs) < -2), aes(x = -2, y = -log10(pvalue)), alpha = 0.5,shape=60,size=4) +
+  # geom_point(data = subset(fisherDF, log10(ORs) > 2), aes(x = 2, y = -log10(pvalue)), alpha = 0.5,shape=62,size=4) +
+  geom_hline(yintercept=-log10(0.05), linetype="dashed", color = "red")+
+  annotate(geom = "text",label="p-value = 0.05",x=-2,y=-log10(0.05),hjust=0,vjust=-0.8)
 
 
 # Psites<-numeric()
