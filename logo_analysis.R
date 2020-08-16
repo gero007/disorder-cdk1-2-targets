edfedc0f9b28b69b51722f4980ba8c681e7a0b2e
gero007 <gero007@gmail.com>
2020-08-16 14:16:22 +0200

    Correction in LOGO function. Motif analysis added

--- logo_analysis.R
+++ logo_analysis.R
@@ -43,17 +43,18 @@ informationContent_calculator <- function(sequences,backgroundProb){
     
     windowsLength <- unique(nchar(sequences))
     sequencesCount <- length(sequences)
-    aux_matrix <- matrix(unlist(strsplit(sequences,"")),byrow = T,nrow=sequencesCount,ncol = windowsLength)
-    aux_matrix <- summary(aux_matrix,maxsum = 100)
+    aux_matrix <- matrix(as.factor(unlist(strsplit(sequences,""))),byrow = T,nrow=sequencesCount,ncol = windowsLength)
+    # not working now. Should return a count sumarrry of each aminoacide
     out_matrix <- matrix(nrow = 20, ncol = windowsLength)
     rownames(out_matrix) <- AA_ALPHABET[1:20]
     
     for (i in 1:ncol(aux_matrix)) {
       
-      colCounts <- str_split_fixed(aux_matrix[,i],":( )*",2)
-      aux_names <- colCounts[,1]
-      colCounts <- as.numeric(trimws(colCounts[,2]))
-      names(colCounts) <- aux_names
+      colCounts <- summary(as.factor(aux_matrix[,i]))
+      # colCounts <- str_split_fixed(aux_matrix[,i],":( )*",2)
+      # aux_names <- colCounts[,1]
+      # colCounts <- as.numeric(trimws(colCounts[,2]))
+      # names(colCounts) <- aux_names
       colCounts <- colCounts[(names(colCounts)!="_" & names(colCounts)!="")]
       ObservedProb <- colCounts/sum(colCounts)
       for (aminoacid in row.names(out_matrix)) {
@@ -107,21 +108,25 @@ ggseqlogo(IC_matrix_list,seq_type='aa',method='custom',ncol = 3) +
   theme( axis.line.y = element_line(size = 0.5, linetype = "solid"),axis.ticks.y = element_line(size = 0.5, linetype = "solid")) + 
   geom_hline(yintercept=0, linetype="solid",size = 0.5)
 
-
+NonProlineDirectedMotifs <- list()
 # Plk: [D/N/E/Y]-X-[S/T]-[ ϕ /F; no P]-[ ϕ /X]
-Plk_motif_re <- "^.{13}[D|N|E|Y].[S|T][V|I||L|F|W|Y|M]" 
+NonProlineDirectedMotifs[["Plk_motif_re"]] <- "^.{13}[D|N|E|Y].[S|T][V|I||L|F|W|Y|M]" 
 # Aurora: R/K-X-S/T-[ ϕ /F; no P]
-Aurora_motif_re <- "^.{13}[R|K].[S|T][V|I||L|F|W|Y|M]" 
+NonProlineDirectedMotifs[["Aurora_motif_re"]] <- "^.{13}[R|K].[S|T][V|I||L|F|W|Y|M]" 
 # Nek: [F/L/M]-X-X-S/T-[ ϕ /F; no P]-[R/H/X]
-Nek_motif_re <- "^.{12}[F|L|M].{2}[S|T][V|I||L|F|W|Y|M][R|H|X]"  
+NonProlineDirectedMotifs[["Nek_motif_re"]] <- "^.{12}[F|L|M].{2}[S|T][V|I||L|F|W|Y|M][R|H|X]"  
 # Casein kinase 1: D/E-D/E-D/E-X-X-S/T-ϕ
-Csk1_motif_re <- "^.{10}[E|D][E|D][E|D].{2}[S|T][A|L|I|V]"
+NonProlineDirectedMotifs[["Csk1_motif_re"]] <- "^.{10}[E|D][E|D][E|D].{2}[S|T][A|L|I|V]"
 # pS/pT-X-X-S/T-ϕ 
-Unknown_motif_re <- "^.{15}[S|T].{2}[S|T][A|L|I|V]"
+NonProlineDirectedMotifs[["Unknown_motif_re"]] <- "^.{15}[S|T].{2}[S|T][A|L|I|V]"
 # Casein kinase 2: S/T-D/E-X-D/E
-Csk1_motif_re <- "^.{15}[S|T][E|D].[E|D]"
+NonProlineDirectedMotifs[["Csk1_motif_re"]] <- "^.{15}[S|T][E|D].[E|D]"
 # PKA: R-R/K-X-S-ϕ
-Pka_motif_re <- "^.{15}[S|T][E|D].[E|D]"
+NonProlineDirectedMotifs[["Pka_motif_re"]] <- "^.{12}R[R|K].S[V|I||L|F|W|Y|M]"
 
 
-phosphoSiteClusters$sequence[grep(Plk_motif_re,phosphoSiteClusters$sequence)]
+sapply(NonProlineDirectedMotifs, function(x){
+                                              length(grep(x,phosphoSiteClusters$sequence))
+                                              })
+
+
