2b2426e8a0b286bfca66a56feca5127ce475126a
Geronimo Dubra <gero007@gmail.com>
2020-08-31 14:16:50 +0200

    fig4a finished. Binomial stats added. Starting position vs disorder plots

--- testJuanDiso.R
+++ testJuanDiso.R
@@ -62,17 +62,17 @@ phosphosites <- phosphosites %>% rename(ID=Proteins,psites=`Positions within pro
 # The data has been opened by excel and some proteins have their names modified to be dates.
 
 phosphosites$psites <- lapply(phosphosites$psites, function(x){ return(as.numeric(strsplit(x,",")[[1]]))})
-
+phosphosites$psites_count <- sapply(phosphosites$psites, length)
 all_predictions_phospho <- merge.data.frame(all_predictions_phospho,phosphosites,by = "ID")
 
 
-phosphoDiso_obs <- list()
-phosphoDiso_expct_uni <- numeric()
-for (i in 1:nrow(all_predictions_phospho)) {
-  diso_fraction <- length(all_predictions_phospho[i,"disordered"][[1]])/nchar(all_predictions_phospho[i,"sequence"])
-  phosphoDiso_expct_uni[i] <- length(all_predictions_phospho[i,"psites"][[1]])*diso_fraction
-  phosphoDiso_obs[[i]] <- sum(all_predictions_phospho[i,"psites"][[1]] %in% all_predictions_phospho[i,"disordered"][[1]])
-}
+# phosphoDiso_obs <- list()
+# phosphoDiso_expct_uni <- numeric()
+# for (i in 1:nrow(all_predictions_phospho)) {
+#   diso_fraction <- length(all_predictions_phospho[i,"disordered"][[1]])/nchar(all_predictions_phospho[i,"sequence"])
+#   phosphoDiso_expct_uni[i] <- length(all_predictions_phospho[i,"psites"][[1]])*diso_fraction
+#   phosphoDiso_obs[[i]] <- sum(all_predictions_phospho[i,"psites"][[1]] %in% all_predictions_phospho[i,"disordered"][[1]])
+# }
 
 
 all_predictions_phospho <- all_predictions_phospho %>% mutate(anova_sig=case_when(
@@ -80,14 +80,16 @@ all_predictions_phospho <- all_predictions_phospho %>% mutate(anova_sig=case_whe
   TRUE ~ "ANOVA Significant"
 ))
 
-all_predictions_phospho <- all_predictions_phospho %>% mutate(cluster=case_when(
-  ID %in% cluster_oscillating ~ "Cluster D",
-  ID %in% cluster_interphase ~ "Cluster C",
-  TRUE ~ "other"
-))
+# Tested. The results coloring the points with the cluster is too confusing. The statistics will be used
+# all_predictions_phospho <- all_predictions_phospho %>% mutate(cluster=case_when(
+#   ID %in% cluster_oscillating ~ "Cluster D",
+#   ID %in% cluster_interphase ~ "Cluster C",
+#   TRUE ~ "other"
+# ))
 
 phosphoDiso_obs <- numeric()
 phosphoDiso_expct <- numeric()
+phosphoDiso_expct_prob <- numeric()
 for (i in 1:nrow(all_predictions_phospho)) {
   # diso_fraction <- length(all_predictions_phospho[i,"disordered"][[1]])/nchar(all_predictions_phospho[i,"sequence"])
   # phosphoDiso_expct_uni[i] <- length(all_predictions_phospho[i,"psites"][[1]])*diso_fraction
@@ -95,86 +97,55 @@ for (i in 1:nrow(all_predictions_phospho)) {
   TStotalIndexes <- as.numeric(gregexpr("S|T", all_predictions_phospho[i,"sequence"])[[1]]) 
   TSinDiso_count <- sum(TStotalIndexes %in% all_predictions_phospho[i,"disordered"][[1]])
   TSinDiso_fraction <- TSinDiso_count/length(TStotalIndexes)
-  phosphoDiso_expct[[i]] <- length(all_predictions_phospho[i,"psites"][[1]])*TSinDiso_fraction
+  phosphoDiso_expct_prob[[i]] <- TSinDiso_fraction
+  phosphoDiso_expct[[i]] <- all_predictions_phospho[i,"psites_count"]*TSinDiso_fraction
   phosphoDiso_obs[[i]] <- sum(all_predictions_phospho[i,"psites"][[1]] %in% all_predictions_phospho[i,"disordered"][[1]])
 }
 
-all_predictions_phospho$psites_expct_diso <- phosphoDiso_expct
 all_predictions_phospho$psites_obsv_diso <- phosphoDiso_obs
+all_predictions_phospho$psites_expct_diso <- phosphoDiso_expct
+all_predictions_phospho$psites_expct_diso_prob <- phosphoDiso_expct_prob
+
+
+
+
+# Binomial test:
+# Calculate percentage of disordered region from all proteins
+
+apply(all_predictions_phospho, 1, function(x){
+  
+  binom.test(x = x$psites_obsv_diso,n = x$psites_count,p = x$psites_expct_diso,
+              alternative = "greater")
+})
+
+all_predictions_phospho <- as.data.table(all_predictions_phospho)
+all_predictions_phospho[,binom := purrr::pmap(.(psites_obsv_diso, psites_count, psites_expct_diso_prob), binom.test, alternative="greater")]
+all_predictions_phospho[,binom_p := mapply("[[", binom, "p.value", SIMPLIFY = T)]
+all_predictions_phospho[,binom_q := mapply(p.adjust, binom_p)]
+all_predictions_phospho[,binom_sig := factor(ifelse(binom_q < 0.05, ifelse(binom_q < 0.01, "1% FDR", "5% FDR"), "n.s."), levels=c("n.s.","5% FDR", "1% FDR")) ]
+
+
 
 ggplot(all_predictions_phospho) + 
-  geom_point(aes(x=psites_obsv_diso,y=psites_expct_diso),size=2,alpha=0.5)+
+  geom_point(aes(x=psites_obsv_diso,y=psites_expct_diso, colour = binom_sig),size=2,alpha=0.6)+
   geom_abline(color="darkslategrey",slope = 1,size=1,linetype = "dashed")+
   ggpubr::theme_classic2() + 
   theme(text = element_text(size=17),legend.position = "none") + 
   scale_x_continuous(limits = c(0, 30),breaks = c(seq(0, 30, by = 5)))+ xlab("Observed phospho S/T in IDR") +
-  scale_y_continuous(limits = c(0, 30),breaks = c(seq(0, 30, by = 5)))+ ylab("Expected phospho S/T in IDR")
+  scale_y_continuous(limits = c(0, 30),breaks = c(seq(0, 30, by = 5)))+ ylab("Expected phospho S/T in IDR") + 
+  scale_colour_manual(values = c("gray","orange","red"))
 
 
-# apply(all_protein_id_phospho, MARGIN=1, FUN=function(x) {
-#   all_predictions[[x[["Protein"]]]] <<- setPhospho(all_predictions[[x[["Protein"]]]] , as.numeric(x[["p-site"]]));
-#   return(invisible());
-# })
+test_df <- as.data.frame(cbind(all_predictions_phospho$positions[[1]],all_predictions_phospho$IUPredScores[[1]]))
+names(test_df) <- c("positions","IUPredScores")
+test_df <- test_df %>% mutate(state=case_when(
+  IUPredScores >= 0.5 ~ "Disordered",
+  TRUE ~ "Structured"
+))
 
-# Get the disordered/ordered phospho counts using countPhosphoDisordered
-# phospho_relative_to_organization<- lapply(all_predictions, countPhosphoDisordered)
-# phospho_relative_to_organization<- lapply(all_predictions, countPhosphoDisordered)
-
-# unlist into data.table
-# phospho_relative_to_organization <- rbindlist(phospho_relative_to_organization, id="protein")
-phospho_relative_to_organization <- rbindlist(phosphoDiso_obs, id="protein")
-
-# Convenient structure for batch processing
-phospho_region <- list(
-  all            = phospho_relative_to_organization,
-  anova_sig      = phospho_relative_to_organization[protein %in% significant_anova],
-  oscillating    = phospho_relative_to_organization[protein %in% cluster_oscillating,],
-  exit_metaphase = phospho_relative_to_organization[protein %in% cluster_exit_metaphase,],
-  nonsignificant = phospho_relative_to_organization[! protein %in% significant_anova,]
-)
-
-
-# Calculate percentage of disordered region from all proteins
-disordered_fraction <- mapply(all_predictions, FUN=function(x) {sum(x$.disordered) / nchar(x$.sequence)})
-
-lapply(phospho_region, function(x) {
-  x[,expected_disordered := (disordered + ordered)  * disordered_fraction[protein] ];
-  x[,expected_ordered := (disordered + ordered)  * (1-disordered_fraction[protein]) ];
-})
-
-
-long_phospho_region_list <- lapply(phospho_region, FUN=melt, id="protein")
-long_phospho_region <- rbindlist(long_phospho_region_list, id="group")
-
-wide_phospho_region <- rbindlist(phospho_region, id="group")
-
-# Binomial test
-wide_phospho_region[,disordered_prop := disordered / (disordered + ordered)]
-wide_phospho_region[,expect_disordered_prop := expected_disordered / (expected_disordered + expected_ordered)]
-wide_phospho_region[,n := disordered + ordered]
-
-wide_phospho_region <- wide_phospho_region[n > 0]
-
-wide_phospho_region[,binom := purrr::pmap(.(disordered, n, expect_disordered_prop), binom.test, alternative="t")]
-wide_phospho_region[,binom_p := mapply("[[", binom, "p.value", SIMPLIFY = T)]
-wide_phospho_region[,binom_q := mapply(p.adjust, binom_p)]
-wide_phospho_region[,binom_sig := factor(ifelse(binom_q < 0.05, ifelse(binom_q < 0.01, "1% FDR", "5% FDR"), "n.s."), levels=c("n.s.","5% FDR", "1% FDR")) ]
-
-
-ggplot(long_phospho_region, aes(x=variable, y=value, fill=variable)) + geom_boxplot() + facet_grid(. ~ group)
-
-ggplot(wide_phospho_region) + 
-  geom_point(aes(x=disordered_prop, y=expect_disordered_prop, color=binom_sig)) + 
-  geom_abline(slope=1, color="red") + 
-  theme_minimal()
-
-ggplot(wide_phospho_region) +
-  geom_point(aes(x=disordered, y=expected_disordered, colour = binom_sig), alpha = 0.5, shape=16) +
-  geom_abline(slope=1, color="red") +
-  facet_grid(. ~ group ) + 
-  scale_colour_manual(values = c("gray","orange","red")) + 
-  theme_minimal()
-
-
-
-
+ggplot(test_df,aes(x=positions,y=IUPredScores)) +
+  geom_line(color="darkslategrey") +
+  geom_segment(aes(color=state,x=positions,xend=positions,y=0,yend=IUPredScores),alpha=0.7,linejoin = "round") +
+  geom_hline(yintercept = 0.5,color="darkslategrey",size=1,linetype = "dashed") +
+  ggpubr::theme_classic2() + 
+  theme(text = element_text(size=17),legend.position = "none")
