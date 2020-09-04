e9f6ff283a51fa97b02df5adf3a40b2de14bb3da
Geronimo Dubra <gero007@gmail.com>
2020-09-04 17:20:24 +0200

    changes in plots. perc_diso and starting with boxplots for obvs vs expct

--- testJuanDiso.R
+++ testJuanDiso.R
@@ -4,6 +4,7 @@ library(data.table)
 library(ggplot2)
 library(ggsci)
 library(scales)
+library(reshape2)
 
 
 
@@ -80,7 +81,12 @@ all_predictions_phospho <- merge.data.frame(all_predictions_phospho,phosphosites
 
 all_predictions_phospho <- all_predictions_phospho %>% mutate(anova_sig=case_when(
   ID %in% significant_anova ~ "ANOVA Significant",
-  TRUE ~ "ANOVA Significant"
+  TRUE ~ "ANOVA Non significant"
+))
+
+all_predictions_phospho <- all_predictions_phospho %>% mutate(cluster=case_when(
+  ID %in% cluster_oscillating ~ "Oscillating",
+  TRUE ~ "other"
 ))
 
 # Tested. The results coloring the points with the cluster is too confusing. The statistics will be used
@@ -128,14 +134,14 @@ all_predictions_phospho[,binom_sig := factor(ifelse(binom_q < 0.05, ifelse(binom
 
 
 ggplot(all_predictions_phospho) + 
-  geom_point(aes(x=psites_obsv_diso,y=psites_expct_diso, colour = binom_sig),size=2,alpha=0.6)+
+  geom_point(aes(x=psites_obsv_diso,y=psites_expct_diso, colour = binom_sig),size=2,alpha=0.80)+
   geom_abline(color="darkslategrey",slope = 1,size=0.5,linetype = "dashed")+
   ggpubr::theme_classic2() + 
-  theme(text = element_text(size=17),legend.position = "none") + 
+  theme(text = element_text(size=15),legend.position = c(0.32,0.82),legend.box.just = "left",legend.box.margin = margin(2, 2, 2, 2),legend.box.background = element_rect(color="darkslategrey"),legend.title = element_text(size = 13)) +
+  guides(color=guide_legend(title="Statistical significance")) +
   scale_x_continuous(limits = c(0, 30),breaks = c(seq(0, 30, by = 5)))+ xlab("Observed phospho S/T in IDR") +
   scale_y_continuous(limits = c(0, 30),breaks = c(seq(0, 30, by = 5)))+ ylab("Expected phospho S/T in IDR") + 
   scale_colour_manual(values = pal_jco()(10)[c(3,2,4)])
-
 
 IUpredScoresPlotGenerator <- function(dataframe){
   plotList <- list()
@@ -161,54 +167,64 @@ IUpredScoresPlotGenerator <- function(dataframe){
   return(plotList)
 }
 
-plotList <- IUpredScoresPlotGenerator(subset(all_predictions_phospho,binom_q < 0.05))
+ggplot(all_predictions_phospho) + 
+  geom_point(aes(x=psites_obsv_diso,y=psites_expct_diso, colour = binom_sig),size=2,alpha=0.80)+
+  geom_abline(color="darkslategrey",slope = 1,size=0.5,linetype = "dashed")+
+  ggpubr::theme_classic2() + 
+  theme(text = element_text(size=15),legend.position = c(0.32,0.82),legend.box.just = "left",legend.box.margin = margin(2, 2, 2, 2),legend.box.background = element_rect(color="darkslategrey"),legend.title = element_text(size = 13)) +
+  guides(color=guide_legend(title="Statistical significance")) +
+  scale_x_continuous(limits = c(0, 30),breaks = c(seq(0, 30, by = 5)))+ xlab("Observed phospho S/T in IDR") +
+  scale_y_continuous(limits = c(0, 30),breaks = c(seq(0, 30, by = 5)))+ ylab("Expected phospho S/T in IDR") + 
+  scale_colour_manual(values = pal_jco()(10)[c(3,2,4)])
 
-pdf("IUpredScores.pdf",width = 15,height = 3)
- for (plot in plotList) {
-   print(plot)
- }
-dev.off()
+# plotList <- IUpredScoresPlotGenerator(subset(all_predictions_phospho,binom_q < 0.05))
+# 
+# pdf("IUpredScores.pdf",width = 15,height = 3)
+#  for (plot in plotList) {
+#    print(plot)
+#  }
+# dev.off()
 
 #######################################  HUMAN (Lila subset) ###############################################################
 
 
 
-disoPath <- "predictions/IUpred_run_xenopus/"
-file.names <- dir(disoPath, pattern =".iupred")
-ids <-unlist(strsplit(x = file.names,split = ".iupred",fixed = T))
-all_predictions <- data.frame(ids)
-colnames(all_predictions) <- "ID"
-all_predictions$ID <- as.character(all_predictions$ID)
-positions <- list()
-scores <- list()
-disordered <- list()
-
-for (n in 1:length(file.names)) {
-  aux_table <- read_delim(paste(disoPath,file.names[n],sep = ""),"\t", escape_double = FALSE, col_names = FALSE,col_types = cols(X1 = col_integer(),X2 = col_character(),X3 = col_double()),comment = "#", trim_ws = TRUE)
-  aux_table <- as.data.frame(aux_table)
-  colnames(aux_table) <- c("POS","RES","IUPRED_SCORE")
-  aux_table$IUPRED_DISO <- aux_table$IUPRED_SCORE>=0.5
-  all_predictions[n,"sequence"] <- paste(aux_table$RES,collapse = "")
-  positions[[n]] <- as.numeric(aux_table$POS)
-  scores[[n]] <- as.numeric(aux_table$IUPRED_SCORE)
-  # all_predictions[n,"phospho"] <- NULL
-  all_predictions[n,"threshold"] <- 0.5
-  disordered[[n]] <- which(aux_table$IUPRED_DISO)
-}
-all_predictions$positions <-positions
-all_predictions$IUPredScores <-scores
-all_predictions$disordered <-disordered
-
-# remove unannotated proteins
-# all_protein_id_phospho <- all_protein_id_phospho[all_protein_id_phospho %in% all_predictions$ID]
-all_predictions_phospho <- subset(all_predictions, ID %in% all_protein_id_phospho)
-
-
-# Introduce phosphosites (from all_proteins variable) using the apply function
-phosphosites <- phosphosites <- read_delim("all_phosphosites_xenopus_nr.tab", "\t", escape_double = FALSE, col_types = cols(`Leading proteins` = col_skip(), Protein = col_skip()), trim_ws = TRUE)
-phosphosites <- phosphosites %>% rename(ID=Proteins,psites=`Positions within proteins`,seqWindow=`Sequence window`,UID=`Unique identifier`) %>% group_by(ID) %>% summarise_at(c("psites","seqWindow","UID"),function(x){paste(x, collapse=",")})
-# The data has been opened by excel and some proteins have their names modified to be dates.
-
-phosphosites$psites <- lapply(phosphosites$psites, function(x){ return(as.numeric(strsplit(x,",")[[1]]))})
-phosphosites$psites_count <- sapply(phosphosites$psites, length)
-all_predictions_phospho <- merge.data.frame(all_predictions_phospho,phosphosites,by = "ID")
+# disoPath <- "predictions/IUpred_run_xenopus/"
+# file.names <- dir(disoPath, pattern =".iupred")
+# ids <-unlist(strsplit(x = file.names,split = ".iupred",fixed = T))
+# all_predictions <- data.frame(ids)
+# colnames(all_predictions) <- "ID"
+# all_predictions$ID <- as.character(all_predictions$ID)
+# positions <- list()
+# scores <- list()
+# disordered <- list()
+# 
+# for (n in 1:length(file.names)) {
+#   aux_table <- read_delim(paste(disoPath,file.names[n],sep = ""),"\t", escape_double = FALSE, col_names = FALSE,col_types = cols(X1 = col_integer(),X2 = col_character(),X3 = col_double()),comment = "#", trim_ws = TRUE)
+#   aux_table <- as.data.frame(aux_table)
+#   colnames(aux_table) <- c("POS","RES","IUPRED_SCORE")
+#   aux_table$IUPRED_DISO <- aux_table$IUPRED_SCORE>=0.5
+#   all_predictions[n,"sequence"] <- paste(aux_table$RES,collapse = "")
+#   positions[[n]] <- as.numeric(aux_table$POS)
+#   scores[[n]] <- as.numeric(aux_table$IUPRED_SCORE)
+#   # all_predictions[n,"phospho"] <- NULL
+#   all_predictions[n,"threshold"] <- 0.5
+#   disordered[[n]] <- which(aux_table$IUPRED_DISO)
+# }
+# all_predictions$positions <-positions
+# all_predictions$IUPredScores <-scores
+# all_predictions$disordered <-disordered
+# 
+# # remove unannotated proteins
+# # all_protein_id_phospho <- all_protein_id_phospho[all_protein_id_phospho %in% all_predictions$ID]
+# all_predictions_phospho <- subset(all_predictions, ID %in% all_protein_id_phospho)
+# 
+# 
+# # Introduce phosphosites (from all_proteins variable) using the apply function
+# phosphosites <- phosphosites <- read_delim("all_phosphosites_xenopus_nr.tab", "\t", escape_double = FALSE, col_types = cols(`Leading proteins` = col_skip(), Protein = col_skip()), trim_ws = TRUE)
+# phosphosites <- phosphosites %>% rename(ID=Proteins,psites=`Positions within proteins`,seqWindow=`Sequence window`,UID=`Unique identifier`) %>% group_by(ID) %>% summarise_at(c("psites","seqWindow","UID"),function(x){paste(x, collapse=",")})
+# # The data has been opened by excel and some proteins have their names modified to be dates.
+# 
+# phosphosites$psites <- lapply(phosphosites$psites, function(x){ return(as.numeric(strsplit(x,",")[[1]]))})
+# phosphosites$psites_count <- sapply(phosphosites$psites, length)
+# all_predictions_phospho <- merge.data.frame(all_predictions_phospho,phosphosites,by = "ID")
