b257b6c9a7741037a6cdf50dd2cab2316089ab5f
Geronimo Dubra <gero007@gmail.com>
2021-03-18 14:24:06 +0100

    sync

---
+++ PSP/AURK/PSP_AURK_parse.R
@@ -0,0 +1,61 @@
+
+# PSP Search Result Legend	 
+# COLUMN HEADER	DESCRIPTION
+# PROTEIN	Primary name in PhosphoSitePlusÂ® (PSP)
+# GENE	Unique ID (UID) assigned to a gene by the HUGO Nomenclature Committee and adapted by HUPO for the cognate protein.
+# ORGANISM	The species from which either the kinase and/or substrate proteins have been derived
+# ACC_ID	Primary accession ID in PSP
+# MW_(DA)	Molecular weight in Daltons.
+# MOD_RSD	The location within the protein sequence of the amino acid residue that is posttranslationally modified (modsite).
+# SITE_+/-7_AA	The modsite plus flanking sequence (+/- 7 rsds.). The modsite, as well as other residues within the flanking sequence that are known to be posttranslationally modified, are displayed in lower-case letters.
+# SITE_GRP_ID	Unique identifier of the modification site and its homologous sites in all proteoforms and species
+# DOMAIN	The Pfam residue in which the modsite is located.
+# METHOD	The experimental method in which kinase-substrate relationships were reported. Vivo = in vivo: determined from reactions within living cells, cell cultures or organisms. Vitro = in vitro: determined from reactions outside of living cellular structures.
+# LTP_LIT	The number of literature records using low-throughput (LTP) experimental techniques in which the modsite has been reported. LTP results may be more reliable than MS2 results.
+# MS2_LIT	The number of literature records using tandem mass spectrometry (MS2) experimental techniques in which the modsite has been observed.
+# MS2_CST	The number of curation sets (CS) derived from MS2 experiments performed at Cell Signaling Technology (CST) in which the modsite has been observed.
+# CST_CAT#	The catalog number(s) of CST antibodies specific for the associated modsite.
+
+#there's overlapping between the targets. I create 13 differents DF and I will merge them
+
+phosphosite_AURA_target_HS <- read_delim("PSP/AURK/aura.tab","\t", escape_double = FALSE, trim_ws = TRUE)
+phosphosite_AURB_target_HS <- read_delim("PSP/AURK/aurb.tab","\t", escape_double = FALSE, trim_ws = TRUE)
+phosphosite_AURC_target_HS <- read_delim("PSP/AURK/aurc.tab","\t", escape_double = FALSE, trim_ws = TRUE)
+
+
+
+
+DF_list <- list(phosphosite_AURA_target_HS,
+                phosphosite_AURB_target_HS,
+                phosphosite_AURC_target_HS
+                )
+
+phosphosite_MAPKFamilyTarget_HS <- Reduce(
+  function(x, y, ...) merge(x, y, by = c("GENE","PROTEIN","ACC#","MOD_RSD"),all = T, ...),
+  DF_list
+)
+
+
+
+aurk_data <- phosphosite_MAPKFamilyTarget_HS %>% unite(col = "KINASE",
+                                                       AURA.KINASE,
+                                                       AURB.KINASE,
+                                                       AURC.KINASE,
+                                                       sep = ",",remove = T,na.rm=T )
+
+
+aurk_data <- aurk_data %>% group_by(`ACC#`,GENE,PROTEIN) %>% summarise_at(c("MOD_RSD","KINASE"),function(x){paste(x, collapse=",")})
+
+aurk_data$AURK_MOD_RSD <- sapply(aurk_data$MOD_RSD, function(x){ return(paste(substr(strsplit(x,",")[[1]],start = 2,stop = 2000),collapse = ","))})
+aurk_data$AURK_KINASE <- sapply(aurk_data$KINASE, function(x){ return(paste(unique(strsplit(x,",")[[1]]),collapse = ","))})
+
+aurk_data$MOD_RSD <- NULL
+aurk_data$KINASE <- NULL
+
+write.table(aurk_data,file = "PSP/AURK/PSP_AURK_Target_HS.tab",quote = F,sep = "\t",row.names = F)
+# algunos valores tienen espacios, por eso el gsub
+# human_data$method <- sapply(human_data$method, function(x){ return(paste(unique(gsub(" ", "", strsplit(x,",")[[1]], fixed = TRUE)),collapse = ","))})
+
+
+
+# The data is exported and mannually curated afterwards. Psites from bibliography were added. All the information is then deposited in the file human_data_curated.tab
