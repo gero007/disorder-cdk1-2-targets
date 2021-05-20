98fe8162427de4ee564279bf3f4ca3618bf5f158
desktop <gero007@gmail.com>
2021-05-20 11:02:04 +0200

    sync

--- humanKinases_mlos.R
+++ humanKinases_mlos.R
@@ -7,18 +7,10 @@ library(tidyr)
 
 MLO_lilaDB_nr <- read_delim("MLO_lilaDB_nr.tab", "\t", escape_double = FALSE, trim_ws = TRUE)
 
-MLO_human <- read_delim("MLO_human.tab", "\t", escape_double = FALSE, trim_ws = TRUE)
+MLO_lilaDB_nr <- MLO_lilaDB_nr %>% group_by(uniprot) %>% summarise_at("MLOs",function(x){paste(x,collapse = ",")})
 
-MLO_lilaDB_expanded <- merge.data.frame(MLO_lilaDB_nr,MLO_human,all = T,by.x = "uniprot",by.y = "ACC#")
+# write.table(MLO_lilaDB_nr,file = "MLO_lilaDB_nr_reformattted.tab",quote = F,sep = "\t",row.names = F)
 
-MLO_lilaDB_expanded <- MLO_lilaDB_expanded %>% unite("MLO", body:MLOs, na.rm = TRUE, remove = TRUE,sep = ",")
+MLO_lilaDB_kinaseTargets <- merge.data.frame(MLO_lilaDB_nr,human_data[,c("ACC#","target","target_mapk","target_aurk","target_plk","target_nek","target_dyrk")],by.x = "uniprot",by.y = "ACC#",all.x = T)
 
-MLO_lilaDB_expanded <- MLO_lilaDB_expanded %>% group_by(uniprot) %>% summarise_at("MLO",function(x){paste(x,collapse = ",")})
-
-MLO_lilaDB_expanded <- subset(MLO_lilaDB_expanded,MLO!="")
-
-write.table(MLO_lilaDB_expanded,file = "MLO_lilaDB_nr_expanded.tab",quote = F,sep = "\t",row.names = F)
-
-MLO_lilaDB_expanded_kinaseTargets <- merge.data.frame(MLO_lilaDB_expanded,human_data[,c("ACC#","target","target_mapk","target_aurk","target_plk","target_nek","target_dyrk")],by.x = "uniprot",by.y = "ACC#",all.x = T)
-
-MLO_lilaDB_expanded_kinaseTargets <- MLO_lilaDB_expanded_kinaseTargets[!is.na(MLO_lilaDB_expanded_kinaseTargets$target),]
+MLO_lilaDB_kinaseTargets <- MLO_lilaDB_kinaseTargets[!is.na(MLO_lilaDB_kinaseTargets$target),]
