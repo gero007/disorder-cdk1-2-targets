4e6ddc9a817771089c352e5d5986bb3b99566cf2
Geronimo Dubra <gero007@gmail.com>
2021-05-26 08:26:08 +0200

    sync

---
+++ mlos_lilaDB.R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(stringr)
+library(readr)
+library(tidyr)
+
+redundant_MLO_lilaDB <-  read_delim("MLO_lilaDB_redundant_20052021.tab", "\t", escape_double = FALSE, trim_ws = TRUE)
+
+MLO_lilaDB_nr <- redundant_MLO_lilaDB %>% group_by(`ACC#`) %>% summarise_at("body",function(x){paste(x,collapse = ",")})
+
+
+
+dynamic_symbol_to_HsUniprot <- read_delim("utrech/ids/dynamic_symbol_to_HsUniprot.tab", "\t", escape_double = FALSE, trim_ws = TRUE)
+
+MLO_lilaDB_nr <- MLO_lilaDB_nr %>% mutate(xenopus=case_when(
+    `ACC#` %in% dynamic_symbol_to_HsUniprot$`Human Uniprot` ~ "Dynamic",
+    TRUE ~ "Non Dynamic"
+))
+
+cdkTargets_uniprot <- read_lines("utrech/ids/humanCDKtargets_uniprot.txt",skip = 1)
+
+MLO_lilaDB_nr <- MLO_lilaDB_nr %>% mutate(human=case_when(
+  `ACC#` %in% cdkTargets_uniprot ~ "Cdk target",
+  TRUE ~ "Non Cdk target"
+))
+
